d8d5baf58c6467862801ab0fd5f10e14eac56c3b
HamidMammadov <hamidvm@code.edu.az>
2022-03-30 19:07:50 +0400

    First Commit

---
+++ Pustok/Pustok/Controllers/BasketController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pustok.DAL;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using Pustok.ViewModels.Basket;
+using Newtonsoft.Json;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Pustok.Controllers
+{
+    public class BasketController : Controller
+    {
+        private readonly AppDbContext _context;
+        public BasketController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            string basket = HttpContext.Request.Cookies["basket"];
+
+            List<BasketVM> basketVMs = null;
+
+            if (basket != null)
+            {
+                basketVMs = JsonConvert.DeserializeObject<List<BasketVM>>(basket);
+            }
+            else
+            {
+                basketVMs = new List<BasketVM>();
+            }
+
+            foreach (BasketVM basketVM in basketVMs)
+            {
+                basketVM.Title = _context.Products.FirstOrDefault(p => p.Id == basketVM.Id).Title;
+                basketVM.MainImage = _context.Products.FirstOrDefault(p => p.Id == basketVM.Id).MainImage;
+                basketVM.Price = _context.Products.FirstOrDefault(p => p.Id == basketVM.Id).Price;
+            }
+
+
+            return View(basketVMs);
+        }
+    }
+}
---
+++ Pustok/Pustok/wwwroot/js/main.js
@@ -0,0 +1,54 @@
+﻿$(document).ready(function () {
+    $(document).on("click", "#productdetailId", function (e) {
+        e.preventDefault();
+        let url = $(this).attr("href");
+        //console.log(url);
+
+        fetch(url).then(res => { return res.text() }).then(data =>
+        {
+            //console.log(data)
+
+            $("#quickModal .product-details-modal").html(data);
+            $("#quickModal").modal(true);
+
+        })
+    })
+
+    $(document).on("click", ".addtobasket", function (e) {
+        e.preventDefault();
+
+        let url = $(this).attr("href");
+        //let quantity = $(".quantity").val();
+        //console.log(quantity)
+
+        fetch(url).then(res =>
+        {
+            //console.log(res)
+
+            //if (res.ok)
+            //{
+            //    alert("Sebete Elave Olundu")
+            //}
+
+            return res.text();
+        }).then(data =>
+        {
+            $(".cart-block").html(data);
+        })
+
+    })
+
+    $(document).on("keyup", ".productcount", function () {
+        let count = $(this).val();
+        let productId = $(this).attr("data-productId");
+
+        fetch("/Product/ChangeBasketProductCount/" + productId + '?count=' + count).then(res => {
+            return res.text();
+        }).then(data => {
+            console.log(data);
+            $(".productTable").html(data);
+        })
+    })
+
+
+})
